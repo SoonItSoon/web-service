156e88a320e8f2625fbe6ee551bba5f59ddd2e45
YoonSoo <fhkaldh@naver.com>
2021-03-18 01:01:07 +0900

    21.03.18

--- firebase.js
+++ firebase.js
@@ -8,6 +8,6 @@ const firebaseConfig = {
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID,
     measurementId: process.env.MEASUREMENT_ID
-  };
+};
   
 export default firebase.initializeApp(firebaseConfig);
