f795b731253fd4e9cd0bf7a01a01b7c5b3fa7d62
YoonSoo <fhkaldh@naver.com>
2021-04-08 05:56:52 +0900

    Home 스타일 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,7 @@
 import React from "react";
 import useTitle from "@unsooks/use-title";
 import locals from "locals";
-import {
-    Container,
-    Button,
-    ButtonGroup,
-    Card,
-    Jumbotron,
-    Carousel,
-} from "react-bootstrap";
+import { Container, Button, Card, Jumbotron, Carousel } from "react-bootstrap";
 import "stylesheets/Home.css";
 import urls from "urls";
 import { useHistory } from "react-router";
@@ -28,53 +21,60 @@ const Home = ({ auth }) => {
     const goSignIn = () => {
         history.push(urls.signin);
     };
+    const goHelp = () => {
+        history.push(urls.help);
+    };
     return (
         <Container className="container home__container">
             <Jumbotron>
-                <h1>Hello, world!</h1>
+                <h1>Home Title</h1>
+                <p>Home Description</p>
                 <p>
-                    This is a simple hero unit, a simple jumbotron-style
-                    component for calling extra attention to featured content or
-                    information.
-                </p>
-                <p>
-                    <Button variant="primary">Learn more</Button>
+                    <Button variant="primary" onClick={goHelp}>
+                        Help
+                    </Button>
                 </p>
             </Jumbotron>
             <Card className="text-dark home__card">
                 {auth ? (
                     <Carousel className="home__carousel">
                         <Carousel.Item className="home__carousel__item">
-                            <Button
-                                className="home__carousel__btn"
-                                onClick={goTimeline}
-                            >
-                                개인 동선
-                            </Button>
+                            <div className="home__carousel__btn-div">
+                                <Button
+                                    className="home__carousel__btn"
+                                    onClick={goTimeline}
+                                >
+                                    개인 동선
+                                </Button>
+                            </div>
                             <Carousel.Caption>
                                 <h3>개인 동선</h3>
                                 <p>날짜별 나의 동선 확인</p>
                             </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item className="home__carousel__item">
-                            <Button
-                                className="home__carousel__btn"
-                                onClick={goInterest}
-                            >
-                                관심분야 정보
-                            </Button>
+                            <div className="home__carousel__btn-div">
+                                <Button
+                                    className="home__carousel__btn"
+                                    onClick={goInterest}
+                                >
+                                    관심분야 정보
+                                </Button>
+                            </div>
                             <Carousel.Caption>
                                 <h3>관심분야 정보</h3>
                                 <p>맞춤형 재난 정보 제공</p>
                             </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item>
-                            <Button
-                                className="home__carousel__btn"
-                                onClick={goSearch}
-                            >
-                                재난문자 검색
-                            </Button>
+                            <div className="home__carousel__btn-div">
+                                <Button
+                                    className="home__carousel__btn"
+                                    onClick={goSearch}
+                                >
+                                    재난문자 검색
+                                </Button>
+                            </div>
                             <Carousel.Caption>
                                 <h3>재난문자 검색</h3>
                                 <p>키워드별 재난문자 검색</p>
