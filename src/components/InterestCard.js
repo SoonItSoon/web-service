5477ca7379957ad2f3f0ccd406e21d64384b98ee
YoonSoo Kim <fhkaldh@naver.com>
2021-04-19 21:14:50 +0900

    관심분야 카드 키워드 UI

--- src/components/InterestCard.js
+++ src/components/InterestCard.js
@@ -1,51 +1,103 @@
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
-import { Button, Card, Modal, Spinner } from "react-bootstrap";
-import Loading from "routes/Loading";
-import useAxios from "@unsooks/use-axios";
+import React, { useEffect, useState } from "react";
+import { Button, Card, Modal, Badge } from "react-bootstrap";
+import { disasterNames } from "values/nameArrays";
 
 const InterestCard = ({ info, handleClick }) => {
     const { id, nickname, interest_string } = info;
 
+    const [interObj, setInterObj] = useState({});
+    const [interStrArray, setInterStrArray] = useState([]);
+    const [initInterStrArray, setInitInterStrArray] = useState(false);
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const badgeVariants = ["primary", "danger", "warning"];
 
-    const [init, setInit] = useState(false);
+    useEffect(() => {
+        setInterObj(JSON.parse(interest_string));
 
-    const { loading, error, data, refetch } = useAxios({
-        url:
-            "http://203.253.25.184:8080/search?start_date=2020-12-18%2020:33:00&disaster=1&name=COVID-19&level=1,3",
-    });
+        if (interObj.mainLocation !== undefined) {
+            // 지역
+            let locationString = "";
+            locationString = `${interObj.mainLocation}`;
+            if (interObj.mainLocation !== "전체") {
+                locationString = `${locationString} ${interObj.subLocation}`;
+            }
+            setInterStrArray([locationString]);
+
+            // 재난 종류
+            let disasterString = "";
+            disasterString = `${disasterNames.main[interObj.disasterIndex]}`;
+            if (interObj.disasterIndex === 1 || interObj.disasterIndex === 4) {
+                disasterString = `${disasterString} (${interObj.disasterSubName})`;
+            }
+            // 지진 추가 키워드
+            if (interObj.disasterIndex === 2) {
+                let eqString = "";
+                eqString = `${interObj.eq_mainLocation}`;
+                if (interObj.eq_mainLocation !== "전체") {
+                    eqString = `${eqString} ${interObj.eq_subLocation}`;
+                }
+                eqString = `${eqString}에서 발생한 규모`;
+                eqString = `${eqString} ${interObj.scale_min} ~ ${interObj.scale_max}의 지진`;
+                disasterString = `${disasterString} (${eqString})`;
+            }
+            setInterStrArray((prev) => [...prev, disasterString]);
+
+            // 키워드
+            let keywordString = "";
+            disasterNames.sub[interObj.disasterIndex].forEach(
+                (element, index) => {
+                    if (interObj["disasterSubLevel" + index]) {
+                        if (keywordString === "") {
+                            keywordString = `${
+                                disasterNames.sub[interObj.disasterIndex][index]
+                            }`;
+                        } else {
+                            keywordString = `${keywordString} ${
+                                disasterNames.sub[interObj.disasterIndex][index]
+                            }`;
+                        }
+                    }
+                }
+            );
+            setInterStrArray((prev) => [...prev, keywordString]);
+
+            setInitInterStrArray(true);
+        }
+    }, [interObj.mainLocation]);
 
     const test = () => {
-        console.log(loading);
-        console.log(data);
-        console.log(error);
+        console.log(interStrArray);
     };
 
     return (
         <>
             <Card id={id} className="interest__card-inner">
                 <Card.Body>
-                    <Card.Title>
-                        <div className="interest__card-inner__title__div">
+                    <div className="interest__card-inner__title__div">
+                        <span className="interest__card-inner__title">
                             {nickname}
-                            <Button variant="danger" onClick={test}>
-                                <FontAwesomeIcon icon={faTrashAlt} />
-                            </Button>
-                        </div>
-                    </Card.Title>
-                    <Card.Text>
-                        {loading ? (
-                            <div>{JSON.stringify(data)}</div>
-                        ) : (
-                            <Spinner animation="border" role="status">
-                                <span className="sr-only">Loading...</span>
-                            </Spinner>
-                        )}
-                    </Card.Text>
+                        </span>
+                        <Button variant="danger" onClick={test}>
+                            <FontAwesomeIcon icon={faTrashAlt} />
+                        </Button>
+                    </div>
+
+                    {initInterStrArray &&
+                        interStrArray.map((element, index) => (
+                            <Badge
+                                key={index}
+                                className={`interest__card-inner__badge badge-color-${index}`}
+                                pill
+                                variant={badgeVariants[index]}
+                                key={index}
+                            >
+                                {element}
+                            </Badge>
+                        ))}
                     <Button
                         className="w-100 interest__card-inner__btn"
                         onClick={handleClick[0]}
@@ -54,6 +106,8 @@ const InterestCard = ({ info, handleClick }) => {
                     </Button>
                 </Card.Body>
             </Card>
+
+            {/* 삭제 버튼 Modal */}
             <Modal
                 id={id}
                 show={show}
